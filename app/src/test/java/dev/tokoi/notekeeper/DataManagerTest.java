b3f01c7b70daa5c7fb4a5b9ba435fcbec8714239
Wesley Tokoi <tokoiwesley@gmail.com>
2020-08-18 06:12:00 +0300

    Add a unit test

---
+++ app/src/test/java/dev/tokoi/notekeeper/DataManagerTest.java
@@ -0,0 +1,12 @@
+package dev.tokoi.notekeeper;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DataManagerTest {
+
+    @Test
+    public void createNewNote() {
+    }
+}
