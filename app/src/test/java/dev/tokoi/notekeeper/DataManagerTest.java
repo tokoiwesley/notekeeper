daa40f36c9eb5c9b04933d86bb53bd63c8b7e8b5
Wesley Tokoi <tokoiwesley@gmail.com>
2020-09-09 02:09:54 +0300

    Implement and run a unit test

--- app/src/test/java/dev/tokoi/notekeeper/DataManagerTest.java
+++ app/src/test/java/dev/tokoi/notekeeper/DataManagerTest.java
@@ -8,5 +8,21 @@ public class DataManagerTest {
 
     @Test
     public void createNewNote() {
+        DataManager dm = DataManager.getInstance();
+        final CourseInfo course = dm.getCourse("android_async");
+        final String noteTitle = "Test note title";
+        final String noteText = "This is the body of my test note";
+
+        int noteIndex = dm.createNewNote();
+        NoteInfo newNote = dm.getNotes().get(noteIndex);
+        newNote.setCourse(course);
+        newNote.setTitle(noteTitle);
+        newNote.setText(noteText);
+
+        NoteInfo compareNote = dm.getNotes().get(noteIndex);
+//        assertSame(newNote, compareNote); // check if two references point to the same object
+        assertEquals(compareNote.getCourse(), course);
+        assertEquals(compareNote.getTitle(), noteTitle);
+        assertEquals(compareNote.getText(), noteText);
     }
 }
